4a6c94779dc416f66396af8e0d69a9ed6fdaa976
Nikhil Kumar <77227336+nikhil-xb@users.noreply.github.com>
2023-03-21 00:50:44 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
-train_path= '/kaggle/input/cars-and-tanks-image-classification/cars_tanks/train/'
-test_path= '/kaggle/input/cars-and-tanks-image-classification/cars_tanks/test/'
+train_path= 'input/cars-and-tanks-image-classification/cars_tanks/train/'
+test_path= 'input/cars-and-tanks-image-classification/cars_tanks/test/'
 MODEL_PATH= '/kaggle/input/vit-base-models-pretrained-pytorch/jx_vit_base_p16_224-80ecf9dd.pth'
 EPOCHS= 10
 n_cpu= os.cpu_count()
